0d8abb5ecb5ae798020030d4ae2b531a709ad94a
tefusion <makotoprogr4mming@gmail.com>
2023-07-08 00:43:06 +0200

    Update to 4.1

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,9 +1,9 @@
 #include "register_types.h"
 
 #include <gdextension_interface.h>
+#include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
-#include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/godot.hpp>
 
 #include "my_node.hpp"
@@ -13,10 +13,8 @@ using namespace godot;
 
 static MySingleton *_my_singleton;
 
-void gdextension_initialize(ModuleInitializationLevel p_level)
-{
-	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
-	{
+void gdextension_initialize(ModuleInitializationLevel p_level) {
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		ClassDB::register_class<MyNode>();
 		ClassDB::register_class<MySingleton>();
 
@@ -25,25 +23,21 @@ void gdextension_initialize(ModuleInitializationLevel p_level)
 	}
 }
 
-void gdextension_terminate(ModuleInitializationLevel p_level)
-{
-	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
-	{
+void gdextension_terminate(ModuleInitializationLevel p_level) {
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
 		Engine::get_singleton()->unregister_singleton("MySingleton");
 		memdelete(_my_singleton);
 	}
 }
 
-extern "C"
-{
-	GDExtensionBool GDE_EXPORT gdextension_init(const GDExtensionInterface *p_interface, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
-	{
-		godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+extern "C" {
+GDExtensionBool GDE_EXPORT gdextension_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-		init_obj.register_initializer(gdextension_initialize);
-		init_obj.register_terminator(gdextension_terminate);
-		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+	init_obj.register_initializer(gdextension_initialize);
+	init_obj.register_terminator(gdextension_terminate);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-		return init_obj.init();
-	}
+	return init_obj.init();
+}
 }
